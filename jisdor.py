9961b6e10ffd18527c48acf0905d092c71f09023
heri santoso <heri.santoso308@gmail.com>
2022-02-11 14:28:16 +0700

    commit 2

--- jisdor.py
+++ jisdor.py
@@ -9,25 +9,19 @@ options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(
     options=options, executable_path="D:\HERI\selenium\dataWebBI\chromedriver.exe")
 driver.get(
-    "https://www.bi.go.id/id/statistik/informasi-kurs/transaksi-bi/Default.aspx")
+    "https://www.bi.go.id/id/statistik/informasi-kurs/jisdor/Default.aspx")
 driver.implicitly_wait(10)
-print(driver.page_source)
+# print(driver.page_source)
 time.sleep(5)
 
 
 datakursbi = pd.read_html(driver.page_source)
-print(datakursbi[0])
-print(datakursbi[1])
+# print(datakursbi[0])
 
-
-filename = "D:\HERI\selenium\dataWebBI\TblRefKurs_" + \
-    (time.strftime("%Y%m%d")+".csv")
-filename2 = "D:\HERI\selenium\dataWebBI\TblKurs__" + \
+filename = "D:\HERI\selenium\dataWebBI\Jisdor_" + \
     (time.strftime("%Y%m%d")+".csv")
 
 datakursbi[0].to_csv(filename, index=False, header=False, sep='|')
-datakursbi[1].to_csv(filename2, index=False, header=False, sep='|')
-
 
 time.sleep(5)
 
